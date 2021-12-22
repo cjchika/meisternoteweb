e76d664ed256400fa5f4e33c5cffcf3470010502
Chijioke Chika <8pluscreative@gmail.com>
2021-12-22 02:02:14 -0800

    updating

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -23,30 +23,64 @@ const tabCommunication = document.getElementById('tab-communication')
 const tabDocumentation = document.getElementById('tab-documentation')
 const tabMeetings = document.getElementById('tab-meetings')
 
+const contentSection = document.querySelector('.tabs-contents')
 const knowledgeContent = document.getElementById('tab-knoweledge-content')
 const communicationContent = document.getElementById('tab-communication-content')
 const documentationContent = document.getElementById('tab-documentation-content')
 const managementContent = document.getElementById('tab-management-content')
 
-// console.dir(tabs);
 
-// for(const tab of tabs) {
-//     if(tab.className === 'active'){
-//         return;
+// const toggleTabs = () => {
+//     tabs.forEach(tab => {
+//         tab.addEventListener("click", (e) => {
+//             // console.log(e.currentTarget.dataset.id);
+//             const tabSelect = e.currentTarget;
+//             console.log(tabSelect);
+//         })
+//     })
+// }
+
+// const knowledgeTab = () => {
+//     if(tabCommunication.className === 'border inactive' && tabDocumentation.className === 'border inactive' && tabMeetings.className === 'border inactive') {
+//         tabKnowledge.classname = 'border active'
+//         knowledgeContent.className = 'visible'
+//         contentSection.append(knowledgeContent)
+//     } else { 
+//         tabKnowledge.className = 'border inactive';
+//         knowledgeContent.className = 'invisible'
+//         contentSection.removeChild(knowledgeContent)
 //     }
 // }
 
-// tabs.forEach(tab => {
-//     console.log(tab);
-//     tab.addEventListener('click', () => {
-//         tab.classList.toggle('active')
-
-// })
-
-// tabCommunication.addEventListener('click', () => {
-//     if(tabCommunication.classList.contains('active')) {
-//         tabCommunication.classList.remove('active')
+// const communicationTab = () => {
+//     if(tabKnowledge.className === 'border inactive' && tabDocumentation.className === 'border inactive' && tabMeetings.className === 'border inactive') {
+//         tabCommunication.classname = 'border active'
+//         communicationContent.className = 'visible'
+//         contentSection.append(communicationContent)
 //     } else {
-//         tabCommunication.classList.add('active')
+//         tabCommunication.className = 'border inactive';
+//         communicationContent.className = 'invisible'
+//         contentSection.removeChild(communicationContent)
 //     }
-// })
+// }
+
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", (e) => {
+        const tabSelect = e.currentTarget;
+        if(tabSelect === tabKnowledge) {
+            tabKnowledge.className = 'border active'
+            knowledgeContent.className = 'visible'
+            
+        } else if(tabSelect === tabCommunication) {
+            tabCommunication.className = 'border active'
+            communicationContent.className = 'visible'
+            contentSection.replaceChild(documentationContent)
+            tabCommunication.className = 'border inactive'
+            communicationContent.className = 'invisible'
+            tabKnowledge.className = 'border inactive'
+            knowledgeContent.className = 'invisible'
+        }
+    })
+})
+
